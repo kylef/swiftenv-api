f790dabed23d3411072932c42e934338271c484b
Kyle Fuller <kyle@fuller.li>
2016-05-24 18:28:06 -0700

    Add basic tests

---
+++ tests.py
@@ -0,0 +1,25 @@
+import unittest
+
+from api import Version
+
+
+class VersionTests(unittest.TestCase):
+    def test_version_not_snapshot(self):
+        version = Version('2.2.0', {})
+        self.assertFalse(version.is_snapshot)
+
+    def test_version_is_snapshot(self):
+        version = Version('2.2.1-SNAPSHOT-2016-04-23-a', {})
+        self.assertTrue(version.is_snapshot)
+
+    def test_version_supports_known_platform(self):
+        version = Version('2.2.0', {'test': 'http://example.com/test.pkg'})
+        self.assertTrue(version.supports_platform('test'))
+
+    def test_version_doesnt_support_unknown_platform(self):
+        version = Version('2.2.0', {})
+        self.assertFalse(version.supports_platform('test'))
+
+    def test_versions(self):
+        versions = Version.versions()
+        self.assertTrue(len(versions) > 0)
